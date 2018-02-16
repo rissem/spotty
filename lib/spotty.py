bb5e31a6dbe9d384c15f13026a56eeb6fcb90dfd
Mike Risse <rissem@gmail.com>
2018-02-16 01:04:25 +0000

    ability to retrieve ip

--- lib/spotty.py
+++ lib/spotty.py
@@ -4,9 +4,31 @@ import os
 import subprocess
 import time
 
+#constantly extend the 'lease' of the instance...
+
 def describe (instance_id):
     sh('aws ec2 describe-instances --instance-id ' + instance_id)
 
+
+def find_instance_id(spot_instance_request_id):
+    instance = None
+    while not instance:
+        result = subprocess.check_output(['aws', 'ec2', 'describe-spot-instance-requests', '--spot-instance-request-ids', spot_instance_request_id])
+        result = result.decode('utf-8')
+        result = json.loads(result)
+        print("RESULT", result)
+        print("RESULT", result['SpotInstanceRequests'][0]['Status']['Code'])
+        if result['SpotInstanceRequests'][0]['Status']['Code'] == 'fulfilled':
+            return result['SpotInstanceRequests'][0]['InstanceId']
+        time.sleep(0.5)
+
+def describe_instance(instance_id):
+    result = subprocess.check_output(['aws', 'ec2', 'describe-instances', '--instance-ids', instance_id])
+    result = result.decode('utf-8')
+    result = json.loads(result)
+    return result
+
+def keep_alive
 
 def launch (ami="ami-52261e32", instance_type="g2.2xlarge"):
     instance_spec = {
@@ -22,8 +44,11 @@ def launch (ami="ami-52261e32", instance_type="g2.2xlarge"):
         f.write(json.dumps(instance_spec).encode('utf-8'))
         f.flush()
         result = subprocess.check_output(['aws', 'ec2', 'request-spot-instances', '--instance-count', '1', '--spot-price', '0.5', '--launch-specification', 'file://' + os.path.join(tempfile.gettempdir(), f.name), '--type', 'one-time'])
-        # should look something like {'SpotInstanceRequests': [{'LaunchSpecification': {'InstanceType': 'g2.2xlarge', 'SecurityGroups': [{'GroupName': 'ssh-only', 'GroupId': 'sg-32aadc76'}], 'Placement': {'AvailabilityZone': 'us-west-1b'}, 'KeyName': 'my_aws_key', 'Monitoring': {'Enabled': False}, 'ImageId': 'ami-52261e32'}, 'SpotInstanceRequestId': 'sir-4jtgf1jg', 'State': 'open', 'ProductDescription': 'Linux/UNIX', 'SpotPrice': '0.500000', 'CreateTime': '2018-02-14T03:24:17.000Z', 'InstanceInterruptionBehavior': 'terminate', 'Type': 'one-time', 'Status': {'Code': 'pending-evaluation', 'UpdateTime': '2018-02-14T03:24:17.000Z', 'Message': 'Your Spot request has been submitted for review, and is pending evaluation.'}}]}
+        result = result.decode('utf-8')
+        result = json.loads(result)
+        request_id = result['SpotInstanceRequests'][0]['SpotInstanceRequestId']
+        # terminate spot instance unless it's kept alive...
+        instance_id = find_instance_id(request_id)
+        print('found instance id', instance_id)
 
-        return json.loads(result.decode('utf-8'))
-
-print(launch())
+# launch()
